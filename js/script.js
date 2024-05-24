fa71c5d03191a6e9e4f9108e03490b6aac8fc319
Pisit Kongnarong <39379814+greenoxz@users.noreply.github.com>
2024-05-24 19:20:24 +0700

    optimization
    ย้ายไฟล์ + เปลี่ยน img เป็น webp

--- js/script.js
+++ js/script.js
@@ -5,3 +5,25 @@ document.addEventListener('DOMContentLoaded', function() {
         elem.classList.add('show');
     });
 });
+
+function calculateAge(birthdate) {
+    const today = new Date();
+    const birthDate = new Date(birthdate);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const monthDifference = today.getMonth() - birthDate.getMonth();
+    if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+}
+
+function updateAge() {
+    const birthdate = '2005-08-01'; // Format: YYYY-MM-DD
+    const ageElement = document.getElementById('age');
+    ageElement.textContent = calculateAge(birthdate);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    updateAge();
+    setInterval(updateAge, 1000 * 60 * 60 * 24); // Update every day
+});
